8018a4f3bb4fdec7a148cae01db4cac104f2bbd1
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2025-03-14 20:35:08 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,15 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+const mult = (a, b) => {
+  return a * b;
+};
+const div = (a, b) => {
+  return a / b;
+};
+
+module.exports = { add, sub, mult, div };
