731f24ddc73219cba1d3d82857f2e75c6fb3055e
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2025-03-15 10:55:16 +0530

    Add files via upload

---
+++ Os_module.js
@@ -0,0 +1,14 @@
+const os = require("os");
+
+//console.log(os.arch()); //returns
+//console.log(os.platform()); //returns platform
+//console.log(os.freemem());
+// let mem = os.totalmem();
+// mem = mem / (1024 * 1024);
+// console.log(mem);
+
+// console.log(os.cpus());
+
+//console.log(os.hostname());
+
+console.log(os);
